c12c33c85f9f990e35b4c6fd88825471cf7d21fd
Leandro7Dev <leandroarti@gmail.com>
2023-12-23 13:47:50 -0300

    big fix img light

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,15 @@ function toggleMode() {
 
     const img = document.querySelector("#profile img")
     if (html.classList.contains("light")) {
-        img.setAttribute("src", "./assets/myphoto1.jpeg")
-    } else {
         img.setAttribute("src", "./assets/myphoto2.jpg")
+    } else {
+        img.setAttribute("src", "./assets/myphoto1.jpeg")
     }
 
     const alt = document.querySelector("#profile img")
     if (html.classList.contains("light")) {
-        img.setAttribute("alt", "Foto de perfil de Leandro sorrindo")
-    } else {
         img.setAttribute("alt", "Foto de Leandro e sua esposa sorrindo")
+    } else {
+        img.setAttribute("alt", "Foto de perfil de Leandro sorrindo")
     }
 }
