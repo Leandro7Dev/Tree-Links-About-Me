d211a09519a335fb4bbe79790ef49b7ba3866453
Leandro7Dev <leandroarti@gmail.com>
2023-12-23 13:38:56 -0300

    transform in my project

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,15 @@ function toggleMode() {
 
     const img = document.querySelector("#profile img")
     if (html.classList.contains("light")) {
-        img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("src", "./assets/myphoto1.jpeg")
     } else {
-        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("src", "./assets/myphoto2.jpg")
     }
 
     const alt = document.querySelector("#profile img")
     if (html.classList.contains("light")) {
-        img.setAttribute("alt", "Foto de Mayk de Óculos de Sol")
+        img.setAttribute("alt", "Foto de perfil de Leandro sorrindo")
     } else {
-        img.setAttribute("alt", "Foto de Maiyk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo")
+        img.setAttribute("alt", "Foto de Leandro e sua esposa sorrindo")
     }
 }
